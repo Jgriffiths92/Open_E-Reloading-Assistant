d5f0198b6eb4d7874850b5df73406fc87eb0dfe1
jgrif <j.griffiths92@gmail.com>
2025-03-21 00:30:57 +1100

    Made Save Button only appear on home view and began working on save dialogue

--- app/src/main/java/com/example/opene_dope/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/opene_dope/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.opene_dope.ui.home
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -31,6 +33,21 @@ class HomeFragment : Fragment() {
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+        binding.fab.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("Save Data Card")
+                .setMessage("This is a popup dialog box.")
+                .setPositiveButton("Save") { dialog: DialogInterface, _: Int ->
+                    dialog.dismiss()
+                }
+                .setNegativeButton("Cancel") { dialog: DialogInterface, _: Int ->
+                    dialog.cancel()
+                }
+
+            val dialog = builder.create()
+            dialog.show()
+
         }
         return root
     }
--- app/src/main/java/com/example/opene_dope/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/opene_dope/ui/home/HomeViewModel.kt
@@ -3,7 +3,6 @@ package com.example.opene_dope.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
