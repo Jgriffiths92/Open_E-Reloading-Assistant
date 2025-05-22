c0ebd177f830b803e44d8783f43378b4a434c566
Jgriffiths92 <j.griffiths92@gmail.com>
2025-05-22 18:35:39 +1000

    Refactor NFC intent handling in MyNfcActivity and update activity class name in buildozer.spec

--- app/src/main/java/com/openedope/open_edope/MyNfcActivity.java
+++ app/src/main/java/com/openedope/open_edope/MyNfcActivity.java
@@ -1,13 +1,14 @@
 package com.openedope.open_edope;
 
 import org.kivy.android.PythonActivity;
-import org.kivy.android.PythonUtil;
 import android.content.Intent;
+import android.os.Bundle;
 
 public class MyNfcActivity extends PythonActivity {
     @Override
     public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        PythonUtil.callPython("on_new_nfc_intent_from_java", intent);
+        // Call Python code via PythonActivity's Python instance
+        org.kivy.android.PythonActivity.mService.getPython().callAttr("on_new_nfc_intent_from_java", intent);
     }
 }
