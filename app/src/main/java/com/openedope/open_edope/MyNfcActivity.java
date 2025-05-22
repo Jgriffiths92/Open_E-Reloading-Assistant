108443bb8179499904a7c043add601448cbc2b50
Jgriffiths92 <j.griffiths92@gmail.com>
2025-05-22 18:23:24 +1000

    Refactor NFC intent handling in MyNfcActivity to streamline PythonUtil usage

--- app/src/main/java/com/openedope/open_edope/MyNfcActivity.java
+++ app/src/main/java/com/openedope/open_edope/MyNfcActivity.java
@@ -1,15 +1,13 @@
 package com.openedope.open_edope;
 
 import org.kivy.android.PythonActivity;
-import org.kivy.android.PythonUtil; // Add this import
+import org.kivy.android.PythonUtil;
 import android.content.Intent;
-import android.os.Bundle;
 
 public class MyNfcActivity extends PythonActivity {
     @Override
     public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        // Use PythonUtil to get the Python instance
-        PythonUtil.getInstance().callAttr("on_new_nfc_intent_from_java", intent);
+        PythonUtil.callPython("on_new_nfc_intent_from_java", intent);
     }
 }
