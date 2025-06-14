5b7d391c3ce71bc97ff1031210bf4dc7b42470cc
Jgriffiths92 <j.griffiths92@gmail.com>
2025-06-14 22:42:47 +1000

    Fix package name in NfcProgressListener for consistency

--- main.py
+++ main.py
@@ -338,7 +338,7 @@ if is_android():
     from jnius import PythonJavaClass, java_method
 
     class NfcProgressListener(PythonJavaClass):
-        __javainterfaces__ = ['com/openedope/open_reloading_assistant/NfcProgressListener']
+        __javainterfaces__ = ['com/openedope/open_e_reloading_assistant/NfcProgressListener']
         __javacontext__ = 'app'
 
         def __init__(self, app):
